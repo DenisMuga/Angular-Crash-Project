9d8069b1ee6071b76bd825e518fd76039cba9dbe
DenisMuga <denismugah5@gmail.com>
2022-04-08 11:12:41 +0300

    feature: creating a component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Goal } from './goal';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-crash';
+  // goals: string[];
+  // goals:Goal[] = [
+  //   {id:1, name:'Watch finding Nemo'},
+  //   {id:2,name:'Buy Cookies'},
+  //   {id:3,name:'Get new Phone Case'},
+  //   {id:4,name:'Get Dog Food'},
+  //   {id:5,name:'Solve math homework'},
+  //   {id:6,name:'Plot my world domination plan'},
+  // ];
+
+
+
+  // constructor(){
+  //   this.goals = ['Watch Finding Nemo', 'Buy Cookies', 'Get New Phone Case', 'Get Dog Food', 'Solve math homework', 'Plot my world domination plan']
+  // }
+
+  
 }
+  
